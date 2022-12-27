161f4ec43d9c4cd8dc35e57c7a939d4c29e6de86
Felice La Rizza <larizza.felice@gmail.com>
2022-12-27 20:58:59 +0100

    3.3  Exercise: Toon lijst van de Parties

---
+++ src/main/java/be/buschop/ap2022b/party/controllers/PartyController.java
@@ -0,0 +1,26 @@
+package be.buschop.ap2022b.party.controllers;
+
+
+import be.buschop.ap2022b.party.model.Party;
+import be.buschop.ap2022b.party.repositories.PartyRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PartyController {
+    private final Logger logger = LoggerFactory.getLogger(PartyController.class);
+
+    @Autowired
+    private PartyRepository partyRepository;
+
+    @GetMapping("/partylist")
+    public String partyList(Model model) {
+        Iterable<Party> parties = partyRepository.findAll();
+        model.addAttribute("parties", parties);
+        return "partylist";
+    }
+}
---
+++ src/main/java/be/buschop/ap2022b/party/repositories/PartyRepository.java
@@ -0,0 +1,20 @@
+package be.buschop.ap2022b.party.repositories;
+
+import be.buschop.ap2022b.party.model.Party;
+import be.buschop.ap2022b.party.model.Venue;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface PartyRepository extends CrudRepository<Party, Integer> {
+
+    Iterable<Party> findByVenue(Venue venue);
+
+    Optional<Party> findFirstByIdLessThanOrderByIdDesc(Integer id);
+
+    Optional<Party> findFirstByOrderByIdDesc();
+
+    Optional<Party> findFirstByIdGreaterThanOrderByIdAsc(Integer id);
+
+    Optional<Party> findFirstByOrderByIdAsc();
+}
