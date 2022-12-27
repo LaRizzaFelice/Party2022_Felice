287f800049eede5714668577395b9f38a08a90f0
Felice La Rizza <larizza.felice@gmail.com>
2022-12-27 21:30:19 +0100

    3.4  Exercise: Toon details voor 1 Party - in orde (zonder STYLING)

--- src/main/java/be/buschop/ap2022b/party/controllers/PartyController.java
+++ src/main/java/be/buschop/ap2022b/party/controllers/PartyController.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class PartyController {
@@ -23,6 +26,44 @@ public class PartyController {
         model.addAttribute("parties", parties);
         return "partylist";
     }
+
+    @GetMapping({"/partydetails", "/partydetails/{id}"})
+    public String partyDetails(Model model,
+                               @PathVariable(required = false) Integer id) {
+        if (id == null) return "partydetails";
+
+        Optional<Party> optionalParty = partyRepository.findById(id);
+        if (optionalParty.isPresent()) {
+            long nrOfPartys = partyRepository.count();
+            model.addAttribute("party", optionalParty.get());
+            model.addAttribute("prevId", id > 1 ? id - 1 : nrOfPartys);
+            model.addAttribute("nextId", id < nrOfPartys ? id + 1 : 1);
+        }
+        return "partydetails";
+    }
+
+    @GetMapping({"/partydetails/{id}/prev"})
+    public String partydetailsPrev(Model model, @PathVariable int id) {
+        Optional<Party> prevPartyFromDb = partyRepository.findFirstByIdLessThanOrderByIdDesc(id);
+        if (prevPartyFromDb.isPresent())
+            return String.format("redirect:/partydetails/%d", prevPartyFromDb.get().getId());
+        Optional<Party> lastPartyFromDb = partyRepository.findFirstByOrderByIdDesc();
+        if (lastPartyFromDb.isPresent())
+            return String.format("redirect:/partydetails/%d", lastPartyFromDb.get().getId());
+        return "partydetails";
+    }
+
+    @GetMapping({"/partydetails/{id}/next"})
+    public String partydetailsNext(Model model, @PathVariable int id) {
+        Optional<Party> nextPartyFromDb = partyRepository.findFirstByIdGreaterThanOrderByIdAsc(id);
+        if (nextPartyFromDb.isPresent())
+            return String.format("redirect:/partydetails/%d", nextPartyFromDb.get().getId());
+        Optional<Party> firstPartyFromDb = partyRepository.findFirstByOrderByIdAsc();
+        if (firstPartyFromDb.isPresent())
+            return String.format("redirect:/partydetails/%d", firstPartyFromDb.get().getId());
+        return "partydetails";
+    }
+
 
 
 }
