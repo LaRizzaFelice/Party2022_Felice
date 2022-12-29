02f88b9ac4ddad0152294dff59cf3bd86c4f77b2
Felice La Rizza <larizza.felice@gmail.com>
2022-12-29 10:06:04 +0100

    2.1.1  Nieuwe Controller en View

---
+++ src/main/java/be/buschop/ap2022b/party/controllers/AdminController.java
@@ -0,0 +1,36 @@
+package be.buschop.ap2022b.party.controllers;
+
+import be.buschop.ap2022b.party.model.Party;
+import be.buschop.ap2022b.party.repositories.PartyRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private Logger logger = LoggerFactory.getLogger(AdminController.class);
+
+    @Autowired
+    private PartyRepository partyRepository;
+
+    @GetMapping("/partyedit/{id}")
+    public String partyEdit(Model model,
+                            @PathVariable Integer id) {
+        if (id == null) return "admin/partyedit";
+
+        Optional<Party> optionalParty = partyRepository.findById(id);
+        if (optionalParty.isPresent()) {
+            model.addAttribute("party", optionalParty.get());
+        }
+        return "admin/partyedit";
+    }
+
+}
