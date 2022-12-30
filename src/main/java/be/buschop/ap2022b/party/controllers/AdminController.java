a2663830d10e407996c3e3d0c768b925daaf1c90
Felice La Rizza <larizza.felice@gmail.com>
2022-12-30 16:00:27 +0100

    7.3  Valideer: name mag niet leeg zijn

--- src/main/java/be/buschop/ap2022b/party/controllers/AdminController.java
+++ src/main/java/be/buschop/ap2022b/party/controllers/AdminController.java
@@ -2,6 +2,8 @@ package be.buschop.ap2022b.party.controllers;
 
 import be.buschop.ap2022b.party.model.Party;
 import be.buschop.ap2022b.party.model.Venue;
+import be.buschop.ap2022b.party.model.Artist;
+import be.buschop.ap2022b.party.repositories.ArtistRepository;
 import be.buschop.ap2022b.party.repositories.PartyRepository;
 import be.buschop.ap2022b.party.repositories.VenueRepository;
 import org.slf4j.Logger;
@@ -11,6 +13,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.WebDataBinder;
+import java.beans.PropertyEditorSupport;
+import java.util.Arrays;
+import java.util.List;
 
 import java.util.Optional;
 
@@ -24,6 +30,9 @@ public class AdminController {
 
     @Autowired
     private VenueRepository venueRepository;
+
+    @Autowired
+    private ArtistRepository artistRepository;
 
     @ModelAttribute("party")
     public Party findParty(@PathVariable(required = false) Integer id) {
@@ -41,6 +50,7 @@ public class AdminController {
                             @PathVariable int id) {
         logger.info("partyEdit " + id);
         model.addAttribute("venues", venueRepository.findAll());
+        model.addAttribute("artists", artistRepository.findAll());
         return "admin/partyedit";
     }
 
@@ -49,12 +59,20 @@ public class AdminController {
                                 @PathVariable int id,
                                 @Valid @ModelAttribute("party") Party party,
                                 BindingResult bindingResult,
-                                @RequestParam int venueId) {
-        logger.info("partyEditPost " + id + " -- new name=" + party.getName());
+                                @RequestParam Integer venueId,
+                                @RequestParam(required = false) Integer[] artistIds)  {
+        logger.info("partyEditPost " + id + " -- new name=" + party.getName() + ", date=" + party.getDate());
+        logger.info("                artists " + Arrays.toString(artistIds));
         if (bindingResult.hasErrors()) {
             model.addAttribute("venues", venueRepository.findAll());
+            model.addAttribute("artists", artistRepository.findAll());
+            party.setArtists(artistRepository.findByIdInIfNotNull(artistIds));
             return "admin/partyedit";
         }
+        if (party.getVenue().getId() != venueId) {
+            party.setVenue(new Venue(venueId));
+        }
+        party.setArtists(artistRepository.findByIdInIfNotNull(artistIds));
         partyRepository.save(party);
         return "redirect:/partydetails/" + id;
     }
@@ -64,15 +82,25 @@ public class AdminController {
         logger.info("partyNew ");
         model.addAttribute("party", new Party());
         model.addAttribute("venues", venueRepository.findAll());
+        model.addAttribute("artists", artistRepository.findAll());
         return "admin/partynew";
     }
 
     @PostMapping("/partynew")
     public String partyNewPost(Model model,
-                               @ModelAttribute("party") Party party,
-                               @RequestParam int venueId) {
+                               @Valid @ModelAttribute("party") Party party,
+                               BindingResult bindingResult,
+                               @RequestParam int venueId,
+                               @RequestParam(required = false) Integer[] artistIds) {
         logger.info("partyNewPost -- new name=" + party.getName() + ", date=" + party.getDate());
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("venues", venueRepository.findAll());
+            model.addAttribute("artists", artistRepository.findAll());
+            party.setArtists(artistRepository.findByIdInIfNotNull(artistIds));
+            return "admin/partynew";
+        }
         party.setVenue(new Venue(venueId));
+        party.setArtists(artistRepository.findByIdInIfNotNull(artistIds));
         Party newParty = partyRepository.save(party);
         return "redirect:/partydetails/" + newParty.getId();
     }
